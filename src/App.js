e84000ddbb03388479b26fd50124a084740ef9a8
pplays33 <pplayps98@gmail.com>
2023-09-17 17:05:20 +0300

    feat: add base config and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <> 
+      <div className='container intro'>
+        <h1 className='titel_logo'> MCN </h1>
+      
+        <p className='intro__text'> 
+            ✨ Текст-заполнитель — это текст, который имеет некоторые характеристики реального письменного текста, но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className='popular__post__container'>
+          <div className='content__post'>
+            <p className='content__text'>
+              Имеет некоторые характеристики реального письменного текста, но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра
+            </p>
+            <a href='#' className='btn'> Читать подробнее </a>
+          </div>
+          <div className='img-post'>
+             <img src={logo} alt='postImg'></img>
+          </div>
+        </div>
+
+        <div className='list-posts'>
+          <div className='post'>
+              <img src={logo} alt='post' className='post_container'></img>
+              <h2 className='titel_post'> характеристики реального письменного текста </h2>
+              <p> но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра </p>
+          </div>
+
+          <div className='post'>
+              <img src={logo} alt='post' className='post_container'></img>
+              <h2 className='titel_post'> характеристики реального письменного текста </h2>
+              <p> но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра </p>
+          </div>
+        </div>
+
+        <div className='list-posts2'>
+          <div className='post'>
+            <img src={logo} alt='post' className='post_container'></img>
+            <h2 className='titel_post'> характеристики реального письменного текста </h2>
+            <p> но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра </p>
+          </div>
+
+          <div className='post'>
+            <img src={logo} alt='post' className='post_container'></img>
+            <h2 className='titel_post'> характеристики реального письменного текста </h2>
+            <p> но является случайным набором слов или сгенерирован иным образом. Его можно использовать для отображения образца шрифтов, создания текста для тестирования или обхода спам-фильтра </p>
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
